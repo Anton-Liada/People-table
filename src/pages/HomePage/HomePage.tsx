6ad1d5f77fc44a51be0489cd3e7bf3c76fb47189
Anton Liada <liadaantom@gmail.com>
2022-12-29 18:26:02 +0200

    img was added to the home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,6 +4,12 @@ export const HomePage: React.FC = () => {
   return (
     <>
       <h2 className="page-title">Home Page</h2>
+      <div className="wrapper-img">
+        <img
+          src="https://media3.giphy.com/media/cjNy9CnBNZvYoh0VaW/giphy.gif?cid=790b7611b48ad7cb8f055862b2e0e84b7714d9e5f30cfa46&rid=giphy.gif&ct=g"
+          alt="welcom home"
+        />
+      </div>
     </>
   );
 };
